25646d2488bde8b7a2a4c720da569bb24d845b07
Marko Kalicanin <markokalic88@gmail.com>
2026-01-29 23:14:09 +0100

    Doavanje izmena na novoj grani

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 pro_menager1=ProductManager()
 sto=Product("sto",200,5)
@@ -13,3 +14,13 @@ pro_menager1.dodavanje(krevet)
 
 pro_menager1.popis()
 pro_menager1.ukupna_vrednost()
+
+cart1=Cart()
+izabrani_sto=Product("sto",200,1)
+izabrana_stolica=Product("stolica",50,4)
+izabrani_krevet=Product("krevet",300,1)
+cart1.dodavanje(izabrani_sto)
+cart1.dodavanje(izabrana_stolica)
+cart1.dodavanje(izabrani_krevet)
+cart1.popis()
+cart1.ukupna_vrednost()
